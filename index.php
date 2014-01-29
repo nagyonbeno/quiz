c8319a729af55c4a65a536c32cac9f17a6c902c6
nagyonbeno <nagyonbeno@gmail.com>
2014-01-29 01:54:57 +0100

    javítás

--- index.php
+++ index.php
@@ -5,19 +5,23 @@ require 'functions.php';
 db_connect();
 $kerdesek = get_kerdes();
 
-
-
-$kerdes_id = $kerdesekek;
+$id = array();
 $valasz_szoveg = array($_POST['nev'],$_POST['nem'],$_POST['buzi']);
-foreach ($valasz_szoveg as $valasz) {
-	insert_valasz($kerdes_id,$valasz);
-}
-
 $kerdesek_szama = count(count_kerdes());
 
 
 include 'views/index.tmpl.php';
 
 
+if(isset($_POST['submit'])){
+
+	for ($i=0; $i < $kerdesek_szama ; $i++) { 
+
+		insert_valasz($id[i],$valasz_szoveg[i]);
+
+	}
+ 	
+ 	echo $kerdesek_szama;
+ }
 
 ?>
--- views/index.tmpl.php
+++ views/index.tmpl.php
@@ -1,26 +1,21 @@
 <?php include 'partials/header.php'; ?>
 
-	<form action="" method="post">
+	<form action="" method="post">	
 
 		<?php foreach($kerdesek as $row) : ?>
-			
-			<?php $kerdesekek = array();
-			 array_push($kerdesekek, $row['kerdes_id']);
-			 print_r($kerdesekek);?>
+		
+			<?php array_push($id,$row['kerdes_id'])?>
+			<p><?=$row['kerdes_id']?></p>
 			<h3><?=$row['kerdes_cim']?></h3>
 			<p><?=$row['kerdes_szoveg']?></p>
 
-
-
 		<?php endforeach; ?>
 
+		<input type="submit" name="submit" value="Elküld!" />
 
-		<input type="submit" value="Elküld!" />
 	</form>
 
-	<?php if($_POST) echo "<p>Sikeresen hozzáadva!</p>" ?>
+
 	
-	<p>kérdések száma:<?=$kerdesek_szama?></p>
-	<?php print_r($valasz_szoveg);?><br />
 
 <?php include 'partials/footer.php'; ?>
